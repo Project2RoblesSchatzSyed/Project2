894b9be00fe4dd8721b021eae7e7de1a8f506f84
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2023-04-05 22:54:33 -0500

    Create site.js

---
+++ js/site.js
@@ -0,0 +1,6 @@
+// Function to display message after submit
+function sayCode() {
+   alert("Thank you for filling out the form! We will be in touch with you soon!");
+}
+
+document.getElementById("submit").onclick = sayCode;
