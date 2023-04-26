46781f91a8da83d569a5ab69e1d4063334b2afcb
radfre <schatz.alex@gmail.com>
2023-04-25 22:07:00 -0500

    added jqury msg to site.js and jqury to socials

--- js/site.js
+++ js/site.js
@@ -1,15 +1,30 @@
-// Function to display message after submit
-function sayCode() {
-   alert("Thank you for filling out the form! We will be in touch with you soon!");
-}
+$(document).ready(function() {
+    // Function to display message after submit
+    function sayCode() {
+        alert("Thank you for filling out the form! We will be in touch with you soon!");
+    }
 
-document.getElementById("submit").onclick = sayCode;
+    $("#submit").on("click", sayCode);
 
-
-    $( function() {
-    $( "#accordion" ).accordion({
+    $("#accordion").accordion({
         collapsible: true,
         active: false,
         heightStyle: "content"
     });
-} );
+});
+
+
+
+// Function to display message after submit
+//function sayCode() {
+//   alert("Thank you for filling out the form! We will be in touch with you soon!");
+//}
+//document.getElementById("submit").onclick = sayCode;
+//
+//    $( function() {
+//    $( "#accordion" ).accordion({
+//        collapsible: true,
+//        active: false,
+//        heightStyle: "content"
+//    });
+//} );
