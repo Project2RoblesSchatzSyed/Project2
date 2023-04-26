e06ba51191df924716c726f8de043f07fe9a1306
radfre <schatz.alex@gmail.com>
2023-04-25 21:11:36 -0500

    added jquery script for accodian

--- js/site.js
+++ js/site.js
@@ -1,14 +1,8 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+    $( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content"
     });
-}
+} );
